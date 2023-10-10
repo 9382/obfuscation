9eb9fdef618b737a0382c2a0e872d98805a95658
9382 <omnomaidan@gmail.com>
2023-10-10 15:55:14 +0100

    Some serializer changes
    still not done yet

--- Python/Serializer.py
+++ Python/Serializer.py
@@ -12,11 +12,11 @@ Statements = [
 	ast.AnnAssign,
 
 	ast.For,
-	ast.AsyncFor, # Unimplemented!
+	# ast.AsyncFor, # Unimplemented!
 	ast.While,
 	ast.If,
 	ast.With,
-	ast.AsyncWith, # Unimplemented!
+	# ast.AsyncWith, # Unimplemented!
 
 	ast.Raise,
 	ast.Try,
@@ -49,9 +49,9 @@ Expressions = [
 	ast.DictComp,
 	ast.GeneratorExp,
 
-	ast.Await, # Unimplemented!
-	ast.Yield, # Unimplemented!
-	ast.YieldFrom, # Unimplemented!
+	# ast.Await, # Unimplemented!
+	# ast.Yield, # Unimplemented!
+	# ast.YieldFrom, # Unimplemented!
 
 	ast.Compare,
 	ast.Call,
@@ -68,7 +68,7 @@ Expressions = [
 
 	ast.Slice,
 	ast.Index,
-	ast.ExtSlice, # Unimplemented!
+	# ast.ExtSlice, # Unimplemented!
 
 	# Not strictly expressions but who cares
 	ast.keyword,
@@ -76,7 +76,9 @@ Expressions = [
 	ast.Load,
 	ast.Store,
 	ast.Del,
+]
 
+Operators = [
 	# UnaryOp
 	ast.Invert,
 	ast.Not,
@@ -116,53 +118,65 @@ UntypedTypes = [
 ]
 
 StatementToID = {}
-i = 0
-for statement in Statements:
-	StatementToID[statement] = i
-	i += 1
-print("Highest statement ID:", i-1)
-
 ExpressionToID = {}
-i = 0
-for expr in Expressions:
-	ExpressionToID[expr] = i
-	i += 1
-print("Highest expression ID:", i-1)
+OperatorToID = {}
+for itemList, referenceList, name in [
+	[Statements, StatementToID, "statement"],
+	[Expressions, ExpressionToID, "expression"],
+	[Operators, OperatorToID, "operator"]]:
+	i = 0
+	for item in itemList:
+		referenceList[item] = i
+		i += 1
+	print("Highest " + name + " ID:", i-1)
 
+class BitWriter:
+	pass
 
-def dictionaryify(Object):
-	objtype = type(Object)
-	if isinstance(Object, ast.AST):
-		out = {}
-		if objtype in Expressions:
-			out["_type"] = ExpressionToID[objtype]
-		elif objtype in Statements:
-			out["_type"] = StatementToID[objtype]
-		elif objtype in UntypedTypes:
-			pass
+class BitReader:
+	pass
+
+def serialize(Object):
+	def simplify(Object):
+		objtype = type(Object)
+		if isinstance(Object, ast.AST):
+			print(objtype, Object._fields)
+			out = []
+			if objtype in Expressions:
+				out.append(ExpressionToID[objtype])
+			elif objtype in Statements:
+				out.append(StatementToID[objtype])
+			elif objtype in Operators:
+				out.append(OperatorToID[objtype])
+			elif objtype in UntypedTypes:
+				pass
+			else:
+				print("[!] I have no bloody idea how to handle the type of", objtype)
+			# print(Object, objtype, Object._fields, Object._attributes)
+			for field in Object._fields:
+				out.append(simplify(getattr(Object, field)))
+			return out
+		elif objtype == tuple:
+			return tuple(simplify(x) for x in Object)
+		elif objtype == list:
+			return list(simplify(x) for x in Object)
+		elif objtype == set:
+			return set(simplify(x) for x in Object)
+		elif objtype == dict:
+			out = {}
+			for key in Object.keys():
+				out[simplify(key)] = simplify(Object[key])
+			return out
 		else:
-			print("[!] I have no bloody idea how to handle the type of", objtype)
-		# print(Object, objtype, Object._fields, Object._attributes)
-		for field in Object._fields:
-			out[dictionaryify(field)] = dictionaryify(getattr(Object, field))
-		return out
-	elif objtype == tuple:
-		return tuple(dictionaryify(x) for x in Object)
-	elif objtype == list:
-		return list(dictionaryify(x) for x in Object)
-	elif objtype == set:
-		return set(dictionaryify(x) for x in Object)
-	elif objtype == dict:
-		out = {}
-		for key in Object.keys():
-			out[dictionaryify(key)] = dictionaryify(Object[key])
-		return out
-	else:
-		# print(":(", Object, objtype)
+			# print(":(", Object, objtype)
+			return Object
+	def serialize(Object):
+		print("I cant serialize yet")
 		return Object
+	return serialize(simplify(Object))
 
 
-print(dictionaryify(ast.parse(r"""
+c = ast.parse(r"""
 def x():
 	print("Run a loop")
 	yield 3
@@ -176,4 +190,6 @@ print("Out:",b)
 print("Next:",next(b))
 print("Next:",next(b))
 print("Next:",next(b))
-""")))
+""")
+print(ast.dump(c))
+print(serialize(c))
